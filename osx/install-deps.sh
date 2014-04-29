8e97dc226755f1a4eacf8579d0def3fe443160e2
Jacob Bennett <me@jakebennett.net>
2014-04-29 11:46:36 -0500

    feat: Place to add dependencies I want to install on a new machine.

---
+++ osx/install-deps.sh
@@ -0,0 +1,12 @@
+#create the CODE FOLDER first before running this
+
+
+# https://github.com/rupa/z
+# z, oh how i love you
+# oh and don't forget to initializet this in the bash_profile
+cd ~/code
+git clone https://github.com/rupa/z.git
+chmod +x ~/code/z/z.sh
+# also consider moving over your current .z file if possible. it's painful to rebuild :)
+
+# z binary is already referenced from .bash_profile
